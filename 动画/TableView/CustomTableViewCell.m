91232f22859295b7ab4816c7d6fc49ac3e3ff89b
tianwan_gaobo <505917250@qq.com>
2017-01-20 13:05:39 +0800

    commit

--- 动画/TableView/CustomTableViewCell.m
+++ 动画/TableView/CustomTableViewCell.m
@@ -6,7 +6,16 @@
 //  Copyright © 2017年 tianwan. All rights reserved.
 //
 
+#define kUIScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kUIScreenHeight  [UIScreen mainScreen].bounds.size.height
 #import "CustomTableViewCell.h"
+
+@interface CustomTableViewCell()
+{
+    UIImageView *_imageView;
+}
+
+@end
 
 @implementation CustomTableViewCell
 
@@ -15,22 +24,50 @@
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getRect) name:@"getRect" object:nil];
     
+    _imageView = [[UIImageView alloc] init];
+    [self.contentView addSubview:_imageView];
+    _imageView.frame = CGRectMake(0, 0, kUIScreenWidth, kUIScreenWidth * 2/3);
+    NSString *imageName = [NSString stringWithFormat:@"%u.jpg",arc4random_uniform(9)+1];
+    _imageView.image = [UIImage imageNamed:imageName];
+    _imageView.contentMode = UIViewContentModeScaleAspectFill;
+    _imageView.clipsToBounds = YES;
 }
 
 - (void)getRect
 {
-    if (self.indexPath.row == 0) {
-        CGRect rectInTableView = [self.tableView rectForRowAtIndexPath:self.indexPath];
-        CGRect rect = [self.tableView convertRect:rectInTableView toView:[self.tableView superview]];
-        NSLog(@"%@",NSStringFromCGRect(rect));
+    CGRect rectInTableView = [self.tableView rectForRowAtIndexPath:self.indexPath];
+    CGRect rect = [self.tableView convertRect:rectInTableView toView:[self.tableView superview]];
+    NSLog(@"%@",NSStringFromCGRect(rect));
+    
+    CGFloat center = rect.origin.y + rect.size.height/2;
+    
+    CGFloat Y = fabs(center - kUIScreenHeight/2);
+    NSLog(@"Y:%f",Y);
+    if (Y > 200) {
+        if (Y > 400) {
+            Y = 400;
+        }
+        CGFloat scale = 1 - ((Y - 150)/250.0) * 0.05;
+        NSLog(@"scale:%f",scale);
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        animation.duration = 0.1;
+        animation.toValue = @(scale);
+        animation.removedOnCompletion = NO;
+        animation.fillMode = kCAFillModeForwards;
+        [_imageView.layer addAnimation:animation forKey:nil];
+    }else {
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        animation.duration = 0.1;
+        animation.toValue = @(1.0);
+        animation.removedOnCompletion = NO;
+        animation.fillMode = kCAFillModeForwards;
+        [_imageView.layer addAnimation:animation forKey:nil];
     }
 }
 
 - (void)setIndexPath:(NSIndexPath *)indexPath
 {
     _indexPath = indexPath;
-    
-    
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
--- 动画/TableView/TableViewMenuController.m
+++ 动画/TableView/TableViewMenuController.m
@@ -21,7 +21,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    _menu = [NSMutableArray array];
+    for (NSInteger i = 0; i < 50; i ++) {
+        NSString *str = [NSString stringWithFormat:@"第%ld行",i];
+        [_menu addObject:str];
+    }
+    [self.tableView reloadData];
+
    
 }
 
@@ -29,12 +36,12 @@
 {
     [super viewDidAppear:animated];
     
-    _menu = [NSMutableArray array];
-    for (NSInteger i = 0; i < 50; i ++) {
-        NSString *str = [NSString stringWithFormat:@"第%ld行",i];
-        [_menu addObject:str];
-    }
-    [self.tableView reloadData];
+//    _menu = [NSMutableArray array];
+//    for (NSInteger i = 0; i < 50; i ++) {
+//        NSString *str = [NSString stringWithFormat:@"第%ld行",i];
+//        [_menu addObject:str];
+//    }
+//    [self.tableView reloadData];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
@@ -69,61 +76,61 @@
     
     cell.tableView = tableView;
     cell.indexPath = indexPath;
-    cell.backgroundColor = [UIColor orangeColor];
-    cell.textLabel.text = _menu[indexPath.row];
+//    cell.backgroundColor = [UIColor orangeColor];
+//    cell.textLabel.text = _menu[indexPath.row];
     
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (indexPath.row == 0) {
-        CGRect rectInTableView = [tableView rectForRowAtIndexPath:indexPath];
-        CGRect rect = [tableView convertRect:rectInTableView toView:[tableView superview]];
-        NSLog(@"rect:%@",NSStringFromCGRect(rect));
-    }
-    return YES;
-}
+//- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    if (indexPath.row == 0) {
+//        CGRect rectInTableView = [tableView rectForRowAtIndexPath:indexPath];
+//        CGRect rect = [tableView convertRect:rectInTableView toView:[tableView superview]];
+//        NSLog(@"rect:%@",NSStringFromCGRect(rect));
+//    }
+//    return YES;
+//}
 
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    CAKeyframeAnimation * animation;
-    
-    animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
-    
-    animation.duration = 0.5;
-    
-    //    animation.delegate = self;
-    
-    animation.removedOnCompletion = YES;
-    
-    animation.fillMode = kCAFillModeForwards;
-    
-    
-    NSMutableArray *values = [NSMutableArray array];
-    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.5, 0.1, 1.0)]];
-    
-    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.2, 1.2, 1.0)]];
-    
-    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.9, 0.9, 1.0)]];
-    
-    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 1.0)]];
-    
-    animation.values = values;
-    animation.timingFunction = [CAMediaTimingFunction functionWithName:@"easeInEaseOut"];
-    
-    [cell.layer addAnimation:animation forKey:nil];
-    
-    if (indexPath.row == 0) {
-        CGRect rectInTableView = [tableView rectForRowAtIndexPath:indexPath];
-        CGRect rect = [tableView convertRect:rectInTableView toView:[tableView superview]];
-        NSLog(@"rect:%@",NSStringFromCGRect(rect));
-    }
-}
+//- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    CAKeyframeAnimation * animation;
+//    
+//    animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+//    
+//    animation.duration = 0.5;
+//    
+//    //    animation.delegate = self;
+//    
+//    animation.removedOnCompletion = YES;
+//    
+//    animation.fillMode = kCAFillModeForwards;
+//    
+//    
+//    NSMutableArray *values = [NSMutableArray array];
+//    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.5, 0.1, 1.0)]];
+//    
+//    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.2, 1.2, 1.0)]];
+//    
+//    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.9, 0.9, 1.0)]];
+//    
+//    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 1.0)]];
+//    
+//    animation.values = values;
+//    animation.timingFunction = [CAMediaTimingFunction functionWithName:@"easeInEaseOut"];
+//    
+//    [cell.layer addAnimation:animation forKey:nil];
+//    
+//    if (indexPath.row == 0) {
+//        CGRect rectInTableView = [tableView rectForRowAtIndexPath:indexPath];
+//        CGRect rect = [tableView convertRect:rectInTableView toView:[tableView superview]];
+//        NSLog(@"rect:%@",NSStringFromCGRect(rect));
+//    }
+//}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return  200;
+    return  kUIScreenWidth * 2/3;
 }
 
 
